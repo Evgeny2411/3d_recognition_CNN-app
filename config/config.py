25a0f8f7cdade463fb2cb3f00bd7efcbdb753595
Yevhenii Borysenko <borisenko1315@gmail.com>
2023-07-21 02:43:23 +0300

    Added setup and config

---
+++ config/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# Directories
+BASE_DIR = Path(__file__).parent.parent.absolute()
+CONFIG_DIR = Path(BASE_DIR, "config")
+DATA_DIR = Path(BASE_DIR, "data")
+IMAGES_DIR = Path(BASE_DIR, "images")
+MODELS_DIR = Path(BASE_DIR, "models")
